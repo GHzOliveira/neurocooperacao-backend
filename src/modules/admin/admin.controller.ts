1dca986dfae2ff6b4df6448104703582944ca218
Guilherme Oliveira <72478821+GHzOliveira@users.noreply.github.com>
2024-05-01 13:14:33 -0300

    Deploy render

--- src/modules/admin/admin.controller.ts
+++ src/modules/admin/admin.controller.ts
@@ -1,6 +1,5 @@
 import { Controller, Get, Query, Res } from "@nestjs/common";
 import { AdminService } from "./admin.service";
-import { Response } from 'express';
 
 @Controller('admin')
 export class AdminController {
@@ -10,13 +9,8 @@ export class AdminController {
     async login(@Query('user') user: string, @Query('password') password: string, @Res() res: Response) {
         const admin = await this.adminService.findAdmin(user, password)
         if (admin) {
-            res.header('Access-Control-Allow-Origin', 'https://jogo-neurocooperacao.vercel.app');
-            res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS');
-            res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-            res.header('Access-Control-Allow-Credentials', 'true');
-            res.json({ status: 200, message: 'Admin found', data: admin });
-        } else {
-            res.json({ status: 404, message: 'Admin not found' });
+            return { status: 200, message: 'Admin found', data: admin }
         }
+        return { status: 404, message: 'Admin not found' }
     }
 }
