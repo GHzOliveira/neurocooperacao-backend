72165029ba784668e62a33ec22ad682ab2fd9964
Guilherme Oliveira <72478821+GHzOliveira@users.noreply.github.com>
2024-05-01 17:03:43 -0300

    heroku subindo

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Server } from 'socket.io';
+import { IoAdapter } from '@nestjs/platform-socket.io';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,12 +9,7 @@ async function bootstrap() {
     origin: '*',
     allowedHeaders: 'Content-Type, Authorization',
   });
+  app.useWebSocketAdapter(new IoAdapter(app));
  await app.listen(process.env.PORT || 3333);
-
-  // const ioServer = new Server(server, {
-  //   cors: {
-  //     origin: '*',
-  //   },
-  // });
 }
 bootstrap();
