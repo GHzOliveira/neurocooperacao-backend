8aa22bd256000a463574a315f2dba01fc355e056
Guilherme Oliveira <72478821+GHzOliveira@users.noreply.github.com>
2024-05-01 14:40:31 -0300

    Deploy render

--- src/env.ts
+++ src/env.ts
@@ -3,5 +3,4 @@ const Joi = require('@hapi/joi');
 export const envSchema = Joi.object({
   DATABASE_URL: Joi.string().required(),
   JWT_SECRET: Joi.string(),
-  PORT: Joi.number().default(3333),
 });
